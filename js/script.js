5ab1285f24f0984b48e568953739574da1784776
demikhovr <demikhovr@gmail.com>
2017-08-22 23:32:05 +0300

    Добавлен оверлей для попапа

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
 var feedbackLink = document.querySelector(".contacts__btn");
-      var feedback = document.querySelector(".modal-feedback");
-      var feedbackClose = document.querySelector(".modal-feedback__close");
+var feedback = document.querySelector(".modal-feedback");
+var feedbackClose = document.querySelector(".modal-feedback__close");
+var modalOverlay  = document.querySelector('.modal-overlay');
 
-      feedbackLink.addEventListener("click", function(event) {
-        event.preventDefault();
-        feedback.classList.add("modal-feedback--show");
-      });
+feedbackLink.addEventListener("click", function(event) {
+  event.preventDefault();
+  feedback.classList.add("modal-feedback--show");
+  modalOverlay.classList.add('modal-overlay--show');
+});
 
-      feedbackClose.addEventListener("click", function(event) {
-        event.preventDefault();
-        feedback.classList.remove("modal-feedback--show");
-      });
+feedbackClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  feedback.classList.remove("modal-feedback--show");
+  modalOverlay.classList.remove("modal-overlay--show");
+});
 
-      window.addEventListener("keydown", function(event) {
-        if (event.keyCode === 27) {
-          if (feedback.classList.contains("modal-feedback--show")) {
-            feedback.classList.remove("modal-feedback--show");
-          }
-        }
-      });
+modalOverlay.addEventListener('click', function (event) {
+  event.preventDefault();
+  modalOverlay.classList.remove('modal-overlay--show');
+  feedback.classList.remove('modal-feedback--show');
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (feedback.classList.contains("modal-feedback--show")) {
+      feedback.classList.remove("modal-feedback--show");
+    }
+  }
+});
